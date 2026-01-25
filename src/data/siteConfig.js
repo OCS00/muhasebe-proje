15c0b64a72332f38e7168a783c866b68f0130e48
onurcemshn <onurcem154@gmail.com>
2026-01-26 02:05:40 +0300

    fix: update phone number in site configuration

--- src/data/siteConfig.js
+++ src/data/siteConfig.js
@@ -6,7 +6,7 @@ export const siteConfig = {
   
   // İletişim Bilgileri
   email: "smmmyavuzsahin@hotmail.com",
-  phone: "+90 (555) 123 45 67",
+  phone: "+90 (506) 892 60 26",
   address: "İHSANİYE MAH. BAHÇELER CAD NO:22 OFİS ROYAL İŞ MERKEZİ KAT:10/122 AKDENİZ / MERSİN" ,
   
   // Sosyal Medya (Varsa)
